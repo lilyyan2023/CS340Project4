5491b8740f75ca865262e5d31e9e86d74f0a3947
Timxuv587 <1430415000@qq.com>
2021-03-14 06:06:19 +0800

    fix

--- Scan.py
+++ Scan.py
@@ -6,42 +6,18 @@ import requests
 #import http
 dict = {}
 def scan(input, output):
-    # dict = {}
     f = open(input, "r")
-    # public_dns = ["208.67.222.222", "1.1.1.1", "8.8.8.8", "8.26.56.26", "9.9.9.9", "64.6.65.6",
-    #               "91.239.100.100", "185.228.168.168", "77.88.8.7", "156.154.70.1", "198.101.242.72",
-    #               "176.103.130.130"]
     for line in f.readlines():
-      #   dict[line] = {"scan_time": time.time()}
-      #   dict[line]["ipv4_addresses"] = []
-      #   dict[line]["ipv6_addresses"] = []
-      #   for dns in public_dns:
-      #       ipv4_add_result = subprocess.check_output(["nslookup", "-type=A", line, dns],
-      # timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-      #       ipv4_adds = ipv4_add_result.split("\n")[2:]
-      #       for ipv4_add in ipv4_adds:
-      #           if ipv4_add.startswith("Address:"):
-      #               ipv4_true_add = ipv4_add.split("\t")[1]
-      #               dict[line]["ipv4_addresses"].append(ipv4_true_add)
-      #       ipv6_add_result = subprocess.check_output(["nslookup", "-type=AAAA", line, dns],
-      #           timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-      #       ipv6_adds = ipv6_add_result.split("\n")[2:]
-      #       for ipv6_add in ipv6_adds:
-      #           if ipv6_add.startswith("Address:"):
-      #               ipv6_true_add = ipv6_add.split("\t")[1]
-      #               dict[line]["ipv6_addresses"].append(ipv6_true_add)
-      #   r = requests.get(line)
-      #   if 'server' in r.headers:
-      #       dict[line]["http_server"] = r.headers['server']
-      #   else:
-      #       dict[line]["http_server"] = None
-      #   insecure_http_result = subprocess.check_output(["curl", "-I", line+":80"],
-      #                       timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-      #   insecure_http = insecure_http_result.split("\n")
-      #   dict[line]["insecure_http"] = False
-      #   for element in insecure_http:
-      #       if element.startswith("HTTP"):
-      #           dict[line]["insecure_http"] = True
+        dict[line] = {}
+        get_scan_time(line)
+        get_ipv4_addresses(line)
+        get_ipv6_addresses(line)
+        get_http_server(line)
+        check_insecure_http(line)
+        get_redirect_to(line)
+        get_hst(line)
+        get_tls_version(line)
+        get_ca(line)
     output_f = open(output, "w")
     json.dump(dict, output_f, sort_keys=True, indent=4)
 
@@ -97,31 +73,54 @@ def check_insecure_http(url):
             dict[url]["insecure_http"] = True
 
 def get_redirect_to(url):
+    global dict
     #https://stackoverflow.com/questions/33684356/how-to-capture-the-output-of-openssl-in-python
     lst = openssl_get_header(url)
     if lst != None:
-        if int(lst[0][9:11]) == 301:
-            return True
+        if int(lst[0][9:12]) == 301:
+            dict[url]["redirect_to_https"] = True
         else:
-            return False
+            dict[url]["redirect_to_https"] = False
     else:
         return None
 
 def get_hst(url):
+    global dict
     lst = openssl_get_header(url)
     if lst != None:
+        while int(lst[0][9:12]) == 301:
+            location = ""
+            for h in lst:
+                if h.split(": ")[0] == "Location":
+                    location = h.split(": ")[1]
+                    break
+            location = location.split("://")[1]
+            if location[-1] == "/":
+                location = location[0:len(location)-1]
+            lst = openssl_get_header(location)
         result = False
         for h in lst:
             if h.split(": ")[0] == "Strict-Transport-Security":
-                result = h.split(": ")[1]
                 result = True
                 break
-        return result
+        dict[url]["hsts"] = result
     else:
         return None
 
 def get_tls_version(url):
-    return []
+    global dict
+    result = []
+    tls = nmap_get_TLS(url)
+    if tls != None:
+        result = []
+    if openssl_get_TLSv1_3(url):
+        result.append("TLSv1.3")
+    dict[url]["hsts"] = result
+
+def get_ca(url):
+    result = openssl_get_ca(url)
+    if result != None:
+        dict[url]["root_ca"] = result
 
 
 def openssl_get_header(url):
@@ -129,8 +128,27 @@ def openssl_get_header(url):
         req = subprocess.Popen(["openssl", "s_client", "-quiet", "-connect", url+":443"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = req.communicate(bytes("GET / HTTP/1.0\r\nHost: " + url+"\r\n\r\n",encoding="utf-8"), timeout=2)
         output = output.decode(errors='ignore').split("\r\n\r\n")[0].split("\r\n")
-        print(output)
         return output
+    except subprocess.TimeoutExpired:
+        return None
+    except Exception as e:
+        print(e)
+        return None
+
+def nmap_get_TLS(url):
+    try:
+        TLS_lst = ["SSLv2", "SSLv3", "TLSv1.0", "TLSv1.1", "TLSv1.2"]
+        req = subprocess.Popen(["nmap", "--script", "ssl-enum-ciphers", "-p", "443", url],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = req.communicate(timeout=10)
+        output = output.decode()
+        lst = output.split('\n|')
+        result = []
+        for h in lst:
+            if h.strip().split(":")[0] in TLS_lst:
+                result.append(h.strip().split(":")[0])
+        return result
+    except subprocess.TimeoutExpired:
+        return None
     except Exception as e:
         print(e)
         return None
@@ -140,9 +158,28 @@ def openssl_get_TLSv1_3(url):
         req = subprocess.Popen(["openssl", "s_client", "-tls1_3", "-connect", url+":443"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = req.communicate(timeout=2)
         output = output.decode(errors='ignore')
-        print(output)
-        return output
+        if "TLSv1.3" in output.split(", "):
+            return True
+        else:
+            return False
+    except subprocess.TimeoutExpired:
+        return None
     except Exception as e:
         print(e)
         return None
 
+def openssl_get_ca(url):
+    try:
+        req = subprocess.Popen(["openssl", "s_client", "-connect", url+":443"],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = req.communicate(timeout=2)
+        output = output.decode(errors='ignore').split("---\n")
+        for line in output:
+            if line[0:17] == "Certificate chain":
+                result = line.split("O = ")[-1].split(",")[0]
+                return result
+        return None
+    except subprocess.TimeoutExpired:
+        return None
+    except Exception as e:
+        print(e)
+        return None
