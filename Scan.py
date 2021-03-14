c9a7fa9d33c817f735d209d7a0d9befb30a8c3d2
lilyyan2023 <56418644+lilyyan2023@users.noreply.github.com>
2021-03-14 11:36:47 -0500

    Update Scan.py

--- Scan.py
+++ Scan.py
@@ -37,14 +37,18 @@ def get_ipv4_addresses(url):
     public_dns = ["208.67.222.222", "1.1.1.1", "8.8.8.8", "8.26.56.26", "9.9.9.9", "64.6.65.6",
                   "91.239.100.100", "185.228.168.168", "77.88.8.7", "156.154.70.1", "198.101.242.72",
                   "176.103.130.130"]
-    for dns in public_dns:
-        ipv4_add_result = subprocess.check_output(["nslookup", "-type=A", url, dns],
-                                                      timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-        ipv4_adds = ipv4_add_result.split("\n")[2:]
-        for ipv4_add in ipv4_adds:
-            if ipv4_add.startswith("Address:"):
-                ipv4_true_add = ipv4_add.split(" ")[1]
-                dict[url]["ipv4_addresses"].append(ipv4_true_add)
+    try:
+        for dns in public_dns:
+            ipv4_add_result = subprocess.check_output(["nslookup", "-type=A", url, dns],
+                                                          timeout=5, stderr=subprocess.STDOUT).decode("utf-8")
+            ipv4_adds = ipv4_add_result.split("\n")[2:]
+            for ipv4_add in ipv4_adds:
+                if ipv4_add.startswith("Address:"):
+                    ipv4_true_add = ipv4_add.split(" ")[1]
+                    dict[url]["ipv4_addresses"].append(ipv4_true_add)
+    except Exception as e:
+        print(e)
+
 
 def get_ipv6_addresses(url):
     global dict
@@ -52,14 +56,17 @@ def get_ipv6_addresses(url):
     public_dns = ["208.67.222.222", "1.1.1.1", "8.8.8.8", "8.26.56.26", "9.9.9.9", "64.6.65.6",
                   "91.239.100.100", "185.228.168.168", "77.88.8.7", "156.154.70.1", "198.101.242.72",
                   "176.103.130.130"]
-    for dns in public_dns:
-        ipv6_add_result = subprocess.check_output(["nslookup", "-type=AAAA", url, dns],
-                                                  timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-        ipv6_adds = ipv6_add_result.split("\n")[2:]
-        for ipv6_add in ipv6_adds:
-            if ipv6_add.startswith("Address:"):
-                ipv6_true_add = ipv6_add.split(" ")[1]
-                dict[url]["ipv6_addresses"].append(ipv6_true_add)
+    try:
+        for dns in public_dns:
+            ipv6_add_result = subprocess.check_output(["nslookup", "-type=AAAA", url, dns],
+                                                      timeout=5, stderr=subprocess.STDOUT).decode("utf-8")
+            ipv6_adds = ipv6_add_result.split("\n")[2:]
+            for ipv6_add in ipv6_adds:
+                if ipv6_add.startswith("Address:"):
+                    ipv6_true_add = ipv6_add.split(" ")[1]
+                    dict[url]["ipv6_addresses"].append(ipv6_true_add)
+    except Exception as e:
+        print(e)
 
 def get_http_server(url):
     global dict
@@ -68,15 +75,19 @@ def get_http_server(url):
         dict[url]["http_server"] = r.headers['server']
     else:
         dict[url]["http_server"] = None
+
 def check_insecure_http(url):
     global dict
-    insecure_http_result = subprocess.check_output(["curl", "-I", url + ":80"],
-                                                   timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-    insecure_http = insecure_http_result.split("\n")
-    dict[url]["insecure_http"] = False
-    for element in insecure_http:
-        if element.startswith("HTTP"):
-            dict[url]["insecure_http"] = True
+    try:
+        insecure_http_result = subprocess.check_output(["curl", "-I", url + ":80"],
+                                                       timeout=5, stderr=subprocess.STDOUT).decode("utf-8")
+        insecure_http = insecure_http_result.split("\n")
+        dict[url]["insecure_http"] = False
+        for element in insecure_http:
+            if element.startswith("HTTP"):
+                dict[url]["insecure_http"] = True
+    except Exception as e:
+        print(e)
 
 def get_redirect_to(url):
     global dict
@@ -202,32 +213,36 @@ def get_rdns_names(url, ipv4_add):
     global dict
     dict[url]["rdns_names:"] = []
     # extract ipv4_add from part B
-    rdns_result = subprocess.check_output(["dig", "-x", ipv4_add],
-                                                   timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-    rdns_list = rdns_result.split("\n")
-    if ";; ANSWER SECTION:" in rdns_list:
-        i = 0
-        while i < len(rdns_list):
-            if rdns_list[i] == ";; ANSWER SECTION:":
-                start = i
-                rdns_list = rdns_list[start + 1::]
-            i += 1
-        j = 0
-        while j < len(rdns_list):
-            print(rdns_list[j].startswith(";;"))
-            if rdns_list[j].startswith(";;"):
-                rdns_list = rdns_list[:j - 1]
-            j += 1
-        for element in rdns_list:
-            rdns_name = element.split("\t")[2][:-1]
-            dict[url]["rdns_names:"].append(rdns_name)
+    try:
+        rdns_result = subprocess.check_output(["dig", "-x", ipv4_add],
+                                                       timeout=5, stderr=subprocess.STDOUT).decode("utf-8")
+        rdns_list = rdns_result.split("\n")
+        if ";; ANSWER SECTION:" in rdns_list:
+            i = 0
+            while i < len(rdns_list):
+                if rdns_list[i] == ";; ANSWER SECTION:":
+                    start = i
+                    rdns_list = rdns_list[start + 1::]
+                i += 1
+            j = 0
+            while j < len(rdns_list):
+                print(rdns_list[j].startswith(";;"))
+                if rdns_list[j].startswith(";;"):
+                    rdns_list = rdns_list[:j - 1]
+                j += 1
+            for element in rdns_list:
+                rdns_name = element.split("\t")[2][:-1]
+                dict[url]["rdns_names:"].append(rdns_name)
+    except Exception as e:
+        print(e)
+
 
 def get_rtt_value(ipv4_add):
     # return one single rtt value
         try:
             rtt_result = subprocess.check_output(["sh", "-c",
                                               '"time echo -e' + "'\x1dclose\x0d'" + '| telnet' +ipv4_add+ '"''']
-                                          , timeout = 2, stderr = subprocess.STDOUT).decode("utf-8")
+                                          , timeout = 5, stderr = subprocess.STDOUT).decode("utf-8")
             for element in rtt_result.split("\n"):
                 if element.startswith("real"):
                     return element.split("\t")[1]
