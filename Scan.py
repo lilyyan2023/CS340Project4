3021e9da55ea91ddb0818cfa11fe94d5aa80d4f9
Qiuyang Xu <qxi0749@moore.wot.eecs.northwestern.edu>
2021-03-14 12:00:32 -0500

    add

--- Scan.py
+++ Scan.py
@@ -15,9 +15,9 @@ def scan(input, output):
         print(url)
         dict[url] = {}
         get_scan_time(url)
-        get_ipv4_addresses(url)
+        #get_ipv4_addresses(url)
         #get_ipv6_addresses(url)
-        #get_http_server(url)
+        get_http_server(url)
         #check_insecure_http(url)
         #get_redirect_to(url)
         #get_hst(url)
@@ -72,7 +72,7 @@ def get_ipv6_addresses(url):
 
 def get_http_server(url):
     global dict
-    r = requests.get(url)
+    r = requests.get("http://"+url)
     if 'server' in r.headers:
         dict[url]["http_server"] = r.headers['server']
     else:
